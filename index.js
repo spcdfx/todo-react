027b4aa02a2f47171fa36a84c32577e9aed99aa2
spcdfx <spaced43@gmail.com>
2018-10-08 21:52:19 +0100

    Initial commit of todo-react app

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './containers/App';
+
+ReactDOM.render(<TodoApp />, document.getElementById('app'))
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+	entry: [
+		path.join(parentDir, 'index.js')
+	],
+	module: {
+		rules: [{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: 'babel-loader'
+		}, {
+			test: /\.(less|css)$/,
+			use: [
+				{loader: "style-loader"},
+				{loader: "css-loder"},
+				{loader: "less-loader"}
+			]
+		}
+		]
+	},
+	output: {
+		path: parentDir + '/dist',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: parentDir,
+		historyApiFallback: true
+	},
+	mode: 'development'
+}
